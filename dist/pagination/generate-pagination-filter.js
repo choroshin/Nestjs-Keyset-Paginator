f24d8d6fd9bc77fb1ac38d8b397e412d095af009
Rasoul Jalali <rasoul.jalali.k@gmail.com>
2022-12-13 16:36:42 +0300

    build project

--- dist/pagination/generate-pagination-filter.js
+++ dist/pagination/generate-pagination-filter.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generatePaginationFilter = void 0;
-const pagination_dto_1 = require("./pagination.dto");
 const generate_mongo_filter_key_name_1 = require("../common/generate-mongo-filter-key-name");
 /**
  * @param  {filterDto[]} filter
@@ -9,45 +8,45 @@ const generate_mongo_filter_key_name_1 = require("../common/generate-mongo-filte
 const generatePaginationFilter = (filter) => {
     const filter_fn = {};
     for (const filterElement of filter) {
-        if (filterElement instanceof pagination_dto_1.filterDto) {
-            // console.log('instanceof filterDto')
-            // console.log('filterElement: ', filterElement)
-            if (filterElement.operator === 'regex') {
-                let exp = new RegExp(filterElement.value, 'i');
-                if (filterElement.mode === 'swm') {
-                    exp = new RegExp('^' + filterElement.value + '.*', 'i');
-                }
-                if (filterElement.mode === 'bnm') {
-                    exp = new RegExp('.*' + filterElement.value + '.*', 'i');
-                }
-                if (filterElement.mode === 'ewm') {
-                    // console.log('EWM\n');
-                    exp = new RegExp(filterElement.value + '.*', 'i');
-                }
-                filter_fn[(0, generate_mongo_filter_key_name_1.generateMongoFilterKeyName)(filterElement.name)] = {
-                    ['$' + filterElement.operator]: exp
-                };
+        /** we do not need to control dto here because we implement validation on DTO side */
+        // if (filterElement instanceof filterDto) {
+        // console.log('instanceof filterDto')
+        // console.log('filterElement: ', filterElement)
+        if (filterElement.operator === 'regex') {
+            let exp = new RegExp(filterElement.value, 'i');
+            if (filterElement.mode === 'swm') {
+                exp = new RegExp('^' + filterElement.value + '.*', 'i');
             }
-            else if (filterElement.operator === 'in' || filterElement.operator === 'nin') {
-                filter_fn[(0, generate_mongo_filter_key_name_1.generateMongoFilterKeyName)(filterElement.name)] = {
-                    ['$' + filterElement.operator]: filterElement.arr_value
-                };
+            if (filterElement.mode === 'bnm') {
+                exp = new RegExp('.*' + filterElement.value + '.*', 'i');
             }
-            else if (filterElement.operator === 'between') {
-                filter_fn[(0, generate_mongo_filter_key_name_1.generateMongoFilterKeyName)(filterElement.name)] = {
-                    $gte: filterElement.arr_value[0],
-                    $lte: filterElement.arr_value[1]
-                };
+            if (filterElement.mode === 'ewm') {
+                // console.log('EWM\n');
+                exp = new RegExp(filterElement.value + '.*', 'i');
             }
-            else
-                filter_fn[(0, generate_mongo_filter_key_name_1.generateMongoFilterKeyName)(filterElement.name)] = {
-                    ['$' + filterElement.operator]: filterElement.value
-                };
+            filter_fn[(0, generate_mongo_filter_key_name_1.generateMongoFilterKeyName)(filterElement.name)] = {
+                ['$' + filterElement.operator]: exp
+            };
         }
-        else {
-            console.warn('filterElement must be instance of filterDto');
-            console.warn('filterElement:\n', filterElement);
+        else if (filterElement.operator === 'in' || filterElement.operator === 'nin') {
+            filter_fn[(0, generate_mongo_filter_key_name_1.generateMongoFilterKeyName)(filterElement.name)] = {
+                ['$' + filterElement.operator]: filterElement.arr_value
+            };
         }
+        else if (filterElement.operator === 'between') {
+            filter_fn[(0, generate_mongo_filter_key_name_1.generateMongoFilterKeyName)(filterElement.name)] = {
+                $gte: filterElement.arr_value[0],
+                $lte: filterElement.arr_value[1]
+            };
+        }
+        else
+            filter_fn[(0, generate_mongo_filter_key_name_1.generateMongoFilterKeyName)(filterElement.name)] = {
+                ['$' + filterElement.operator]: filterElement.value
+            };
+        // } else {
+        //     console.warn('filterElement must be instance of filterDto')
+        //     console.warn('filterElement:\n', filterElement)
+        // }
     }
     return filter_fn;
 };
